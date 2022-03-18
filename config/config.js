07b03343c231503f24daf41af099889601280d79
camilo velandia <davidcamilo96000@gmail.com>
2022-03-17 22:05:34 -0500

    db

--- config/config.js
+++ config/config.js
@@ -11,11 +11,11 @@ types.setTypeParser(1114, function(stringValue) {
 });
 
 const databaseConfig = {
-    'host': '127.0.0.1',
+    'host': 'ec2-44-194-167-63.compute-1.amazonaws.com',
     'port': 5432,
-    'database': 'chat_db',
-    'user': 'postgres',
-    'password': '1498'
+    'database': 'd3f3qbbcp370ng',
+    'user': 'xfbmjofzawahpj',
+    'password': 'c194cca6a69cc082bc74dfff05ca8b2b72d253bc2e23443a2fb219bbe2ff860f'
 };
 
 const db = pgp(databaseConfig);
